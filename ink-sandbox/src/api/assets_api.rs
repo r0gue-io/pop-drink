d16f01f6b2e50121e9c8d9b81e436a96534173a1
chungquantin <56880684+chungquantin@users.noreply.github.com>
2024-09-23 20:24:25 +0700

    feat: add approve, total_supply

--- ink-sandbox/src/api/assets_api.rs
+++ ink-sandbox/src/api/assets_api.rs
@@ -1,4 +1,10 @@
-use frame_support::{sp_runtime::DispatchError, traits::{fungibles::{Inspect, Create, Mutate}}};
+use frame_support::{
+    sp_runtime::DispatchError,
+    traits::fungibles::{
+        approvals::{Inspect as ApprovalsInspect, Mutate as ApprovalsMutate},
+        Create, Inspect, Mutate,
+    },
+};
 use pallet_assets::Instance1;
 
 use crate::{AccountIdFor, Sandbox};
@@ -7,15 +13,18 @@ type AssetIdOf<T> = <AssetsOf<T> as Inspect<<T as frame_system::Config>::Account
 type AssetsOf<T> = pallet_assets::Pallet<T, Instance1>;
 type BalanceOf<T> = <AssetsOf<T> as Inspect<<T as frame_system::Config>::AccountId>>::Balance;
 
-// TODO: api is the same with balances api. Perhaps should be changed to e.g. `create_asset`.
-//  Not important for now though.
 /// Assets API for the sandbox.
 pub trait AssetsAPI<T: Sandbox>
-    where
-        T: Sandbox,
-        T::Runtime: pallet_assets::Config<Instance1>,
+where
+    T: Sandbox,
+    T::Runtime: pallet_assets::Config<Instance1>,
 {
-    /// Create a token.
+    /// Creates `value` amount of tokens and assigns them to `account`, increasing the total supply.
+    ///
+    /// # Arguments
+    /// * `id` - ID of the new asset to be created.
+    /// * `owner` - The owner of the created asset.
+    /// * `min_balance` - The asset amount one account need at least.
     fn create(
         &mut self,
         id: &AssetIdOf<T::Runtime>,
@@ -23,12 +32,28 @@ pub trait AssetsAPI<T: Sandbox>
         min_balance: BalanceOf<T::Runtime>,
     ) -> Result<(), DispatchError>;
 
-    /// Mint tokens to an account.
+    /// Approves `spender` to spend `value` amount of tokens on behalf of the caller.
+    ///
+    /// Successive calls of this method overwrite previous values.
     ///
     /// # Arguments
+    /// * `asset` - ID of the asset.
+    /// * `spender` - The account that is allowed to spend the tokens.
+    /// * `value` - The number of tokens to approve.
+    fn approve(
+        &mut self,
+        asset: &AssetIdOf<T::Runtime>,
+        owner: &AccountIdFor<T::Runtime>,
+        delegate: &AccountIdFor<T::Runtime>,
+        amount: BalanceOf<T::Runtime>,
+    ) -> Result<(), DispatchError>;
+
+    /// Creates `value` amount of tokens and assigns them to `account`, increasing the total supply.
     ///
-    /// * `address` - The address of the account to add tokens to.
-    /// * `amount` - The number of tokens to add.
+    /// # Arguments
+    /// * `asset` - ID of the asset.
+    /// * `account` - The account to be credited with the created tokens.
+    /// * `value` - The number of tokens to mint.
     fn mint_into(
         &mut self,
         asset: &AssetIdOf<T::Runtime>,
@@ -36,22 +61,53 @@ pub trait AssetsAPI<T: Sandbox>
         value: BalanceOf<T::Runtime>,
     ) -> Result<BalanceOf<T::Runtime>, DispatchError>;
 
-    /// Return the balance of an account.
+    /// Returns the account balance for the specified `owner`.
     ///
     /// # Arguments
-    ///
-    /// * `address` - The address of the account to query.
-    fn balance_of(&mut self, asset: &AssetIdOf<T::Runtime>, owner: &AccountIdFor<T::Runtime>) -> BalanceOf<T::Runtime>;
+    /// * `owner` - The account whose balance is being queried.
+    fn balance_of(
+        &mut self,
+        asset: &AssetIdOf<T::Runtime>,
+        owner: &AccountIdFor<T::Runtime>,
+    ) -> BalanceOf<T::Runtime>;
 
+    /// Returns the total supply of the `asset`.
+    ///
+    /// # Arguments
+    /// * `asset` - ID of the asset.
+    fn total_supply(&mut self, asset: &AssetIdOf<T::Runtime>) -> BalanceOf<T::Runtime>;
+
+    /// Returns the allowance for a `spender` approved by an `owner`.
+    ///
+    /// # Arguments
+    /// * `asset` - ID of the asset.
+    /// * `owner` - The account that owns the tokens.
+    /// * `spender` - The account that is allowed to spend the tokens.
+    fn allowance(
+        &mut self,
+        asset: &AssetIdOf<T::Runtime>,
+        owner: &AccountIdFor<T::Runtime>,
+        delegate: &AccountIdFor<T::Runtime>,
+    ) -> BalanceOf<T::Runtime>;
+
+    /// Check if the asset is created.
+    ///
+    /// # Arguments
+    /// * `asset` - ID of the asset.
     fn asset_exists(&mut self, asset: &AssetIdOf<T::Runtime>) -> bool;
 }
 
 impl<T> AssetsAPI<T> for T
-    where
-        T: Sandbox,
-        T::Runtime: pallet_assets::Config<Instance1>,
+where
+    T: Sandbox,
+    T::Runtime: pallet_assets::Config<Instance1>,
 {
-    fn create(&mut self, id: &AssetIdOf<T::Runtime>, owner: &AccountIdFor<T::Runtime>, min_balance: BalanceOf<T::Runtime>) -> Result<(), DispatchError> {
+    fn create(
+        &mut self,
+        id: &AssetIdOf<T::Runtime>,
+        owner: &AccountIdFor<T::Runtime>,
+        min_balance: BalanceOf<T::Runtime>,
+    ) -> Result<(), DispatchError> {
         self.execute_with(|| <pallet_assets::Pallet::<T::Runtime, Instance1> as Create<AccountIdFor<T::Runtime>>>::create(id.clone(), owner.clone(), true, min_balance))
     }
 
@@ -61,15 +117,63 @@ impl<T> AssetsAPI<T> for T
         account: &AccountIdFor<T::Runtime>,
         value: BalanceOf<T::Runtime>,
     ) -> Result<BalanceOf<T::Runtime>, DispatchError> {
-        self.execute_with(|| pallet_assets::Pallet::<T::Runtime, Instance1>::mint_into(asset.clone(), account, value))
+        self.execute_with(|| {
+            pallet_assets::Pallet::<T::Runtime, Instance1>::mint_into(asset.clone(), account, value)
+        })
     }
 
-    fn balance_of(&mut self, asset: &AssetIdOf<T::Runtime>, owner: &AccountIdFor<T::Runtime>) -> BalanceOf<T::Runtime> {
-        self.execute_with(|| pallet_assets::Pallet::<T::Runtime, Instance1>::balance(asset.clone(), owner))
+    fn approve(
+        &mut self,
+        asset: &AssetIdOf<T::Runtime>,
+        owner: &AccountIdFor<T::Runtime>,
+        delegate: &AccountIdFor<T::Runtime>,
+        amount: BalanceOf<T::Runtime>,
+    ) -> Result<(), DispatchError> {
+        self.execute_with(|| {
+            pallet_assets::Pallet::<T::Runtime, Instance1>::approve(
+                asset.clone(),
+                owner,
+                delegate,
+                amount,
+            )
+        })
+    }
+
+    fn balance_of(
+        &mut self,
+        asset: &AssetIdOf<T::Runtime>,
+        owner: &AccountIdFor<T::Runtime>,
+    ) -> BalanceOf<T::Runtime> {
+        self.execute_with(|| {
+            pallet_assets::Pallet::<T::Runtime, Instance1>::balance(asset.clone(), owner)
+        })
+    }
+
+    fn total_supply(&mut self, asset: &AssetIdOf<T::Runtime>) -> BalanceOf<T::Runtime> {
+        self.execute_with(|| {
+            pallet_assets::Pallet::<T::Runtime, Instance1>::total_supply(asset.clone())
+        })
+    }
+
+    fn allowance(
+        &mut self,
+        asset: &AssetIdOf<T::Runtime>,
+        owner: &AccountIdFor<T::Runtime>,
+        delegate: &AccountIdFor<T::Runtime>,
+    ) -> BalanceOf<T::Runtime> {
+        self.execute_with(|| {
+            pallet_assets::Pallet::<T::Runtime, Instance1>::allowance(
+                asset.clone(),
+                owner,
+                delegate,
+            )
+        })
     }
 
     fn asset_exists(&mut self, asset: &AssetIdOf<T::Runtime>) -> bool {
-        self.execute_with(|| pallet_assets::Pallet::<T::Runtime, Instance1>::asset_exists(asset.clone()))
+        self.execute_with(|| {
+            pallet_assets::Pallet::<T::Runtime, Instance1>::asset_exists(asset.clone())
+        })
     }
 }
 
@@ -81,19 +185,12 @@ mod test {
     fn api_works() {
         let mut sandbox = DefaultSandbox::default();
         let token = 1;
-        let balance = sandbox.balance_of(&token, &DefaultSandbox::default_actor());
+        let actor = DefaultSandbox::default_actor();
+        let balance = sandbox.balance_of(&token, &actor);
 
-        sandbox
-            .create(&token, &DefaultSandbox::default_actor(), 1)
-            .unwrap();
-        sandbox
-            .mint_into(&token, &DefaultSandbox::default_actor(), 100)
-            .unwrap();
-
-        assert_eq!(
-            sandbox.balance_of(&token, &DefaultSandbox::default_actor()),
-            balance + 100
-        );
+        sandbox.create(&token, &actor, 1).unwrap();
+        sandbox.mint_into(&token, &actor, 100).unwrap();
+        assert_eq!(sandbox.balance_of(&token, &actor), balance + 100);
 
         assert!(sandbox.asset_exists(&token));
     }
