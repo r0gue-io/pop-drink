b17157f676521ce2acc9f44c4ca06e7291f0a7f8
chungquantin <56880684+chungquantin@users.noreply.github.com>
2024-10-10 00:00:22 +0700

    fix: resolve comments

--- crates/pop-drink/src/error.rs
+++ crates/pop-drink/src/error.rs
@@ -6,169 +6,144 @@ pub use drink::{
 	pallet_assets::Error as AssetsError, pallet_balances::Error as BalancesError,
 	pallet_contracts::Error as ContractsError,
 };
-pub use pop_api::primitives::v0::*;
 use scale::{Decode, Encode};
 
-use crate::utils::decode;
-
-/// Configuration for the runtime error.
-pub trait RuntimeErrorConfig: Debug {
-	/// Error type of the runtime modules.
-	/// Reference: https://paritytech.github.io/polkadot-sdk/master/solochain_template_runtime/enum.RuntimeError.html.
-	type ModuleError: Decode + Encode + Debug;
-	/// Error type of the Pop API depends on the version.
-	type PopApiError: Decode + Encode + Debug + From<u32> + Into<u32>;
-	/// Index of the variant `RuntimeError::Module`. This is based on the index of
-	/// `PopApiError::Module`.
-	const MODULE_INDEX: u8;
+fn decode<T: Decode>(data: &[u8]) -> T {
+	T::decode(&mut &data[..]).expect("Decoding failed")
 }
 
 /// Runtime error for testing.
+///
+/// # Generic Parameters:
+///
+/// - `ModuleError` - Error type of the runtime modules. Reference: https://paritytech.github.io/polkadot-sdk/master/solochain_template_runtime/enum.Error.html.
+/// - `ApiError` - Error type of the API, which depends on version.
+/// - `MODULE_INDEX` - Index of the variant `Error::Module`. This is based on the index of
+///   `ApiError::Module`
 #[derive(Encode, Decode, Debug)]
-pub enum RuntimeError<T: RuntimeErrorConfig> {
+pub enum Error<ModuleError, ApiError, const MODULE_INDEX: u8>
+where
+	ModuleError: Decode + Encode + Debug,
+	ApiError: Decode + Encode + Debug + From<u32> + Into<u32>,
+{
 	/// Module errors of the runtime.
-	Module(T::ModuleError),
-	/// Every `PopApiError`.
-	Raw(T::PopApiError),
+	Module(ModuleError),
+	/// Every `ApiError`.
+	Api(ApiError),
 }
 
-impl<T: RuntimeErrorConfig> From<RuntimeError<T>> for u32 {
-	/// Converts a `RuntimeError` into a numerical value of `PopApiError`.
+impl<ModuleError, ApiError, const MODULE_INDEX: u8> From<Error<ModuleError, ApiError, MODULE_INDEX>>
+	for u32
+where
+	ModuleError: Decode + Encode + Debug,
+	ApiError: Decode + Encode + Debug + From<u32> + Into<u32>,
+{
+	/// Converts a `Error` into a numerical value of `ApiError`.
 	///
-	/// This conversion is necessary for comparing `RuntimeError` instances with other types
-	/// that implement `Into<u32>`, as `RuntimeError` does not implement `Eq`.
-	/// Use this function to obtain a numerical representation of the error for comparison or
-	/// further processing.
-	fn from(error: RuntimeError<T>) -> Self {
-		let pop_api_error = match error {
-			RuntimeError::Module(error) => {
+	/// This conversion is necessary for comparing `Error` instances with other types.
+	// Compared types must implement `Into<u32>`, as `Error` does not implement `Eq`.
+	// Use this function to obtain a numerical representation of the error for comparison or
+	// further processing.
+	fn from(error: Error<ModuleError, ApiError, MODULE_INDEX>) -> Self {
+		match error {
+			Error::Module(error) => {
 				let mut encoded = error.encode();
-				encoded.insert(0, T::MODULE_INDEX);
+				encoded.insert(0, MODULE_INDEX);
 				encoded.resize(4, 0);
-				decode::<T::PopApiError>(&encoded)
+				decode::<ApiError>(&encoded)
 			},
-			RuntimeError::Raw(error) => decode::<T::PopApiError>(&error.encode()),
-		};
-		pop_api_error.into()
+			Error::Api(error) => decode::<ApiError>(&error.encode()),
+		}
+		.into()
 	}
 }
 
-impl<T: RuntimeErrorConfig> From<u32> for RuntimeError<T> {
-	/// Converts a numerical value of `PopApiError` into a `RuntimeError`.
+impl<ModuleError, ApiError, const MODULE_INDEX: u8> From<u32>
+	for Error<ModuleError, ApiError, MODULE_INDEX>
+where
+	ModuleError: Decode + Encode + Debug,
+	ApiError: Decode + Encode + Debug + From<u32> + Into<u32>,
+{
+	/// Converts a numerical value of `ApiError` into a `Error`.
 	///
-	/// This is used to reconstruct and display a `RuntimeError` from its numerical representation
+	/// This is used to reconstruct and display a `Error` from its numerical representation
 	/// when an error is thrown.
 	fn from(value: u32) -> Self {
-		let error = T::PopApiError::from(value);
+		let error = ApiError::from(value);
 		let encoded = error.encode();
-		if encoded[0] == T::MODULE_INDEX {
+		if encoded[0] == MODULE_INDEX {
 			let (index, module_error) = (encoded[1], &encoded[2..]);
 			let data = vec![vec![index], module_error.to_vec()].concat();
-			return RuntimeError::Module(decode(&data));
+			return Error::Module(decode(&data));
 		}
-		RuntimeError::Raw(error)
+		Error::Api(error)
 	}
 }
 
-pub mod v0 {
-	use std::fmt::Debug;
-	/// A utility macro to assert that an error returned from a smart contract method matches the
-	/// `RuntimeError`.
-	///
-	/// # Generic parameters:
-	///
-	/// - `R` - Success type returned if Ok().
-	/// - `E` - Returned `Err()` value of a method result. Must be convertable to `u32`.
-	/// - `RuntimeError` - Runtime error type.
-	///
-	/// # Parameters:
-	///
-	/// - `result` - Result returned by a smart contract method.
-	/// - `expected_error` - `RuntimeError` to be asserted.
-	pub fn assert_runtime_err_inner<R, E, RuntimeError>(
-		result: Result<R, E>,
-		expected_error: RuntimeError,
-	) where
-		E: Into<u32>,
-		RuntimeError: From<u32> + Into<u32> + Debug,
-	{
-		crate::error::assert_runtime_err_inner::<pop_primitives::Error, R, E, RuntimeError>(
-			result,
-			expected_error,
-		)
-	}
-
-	/// A utility macro to assert that an error returned from a smart contract method using Pop API
-	/// V0.
-	///
-	/// # Parameters:
-	///
-	/// - `result` - The result returned by a smart contract method. It is of type `Result<R, E>`,
-	///   where the error type `E` must implement a conversion to `u32`.
-	/// - `error` - A `RuntimeError` type configured specifically for the Pop API V0.
-	///
-	/// # Example:
-	///
-	/// ```rs
-	/// use drink::devnet::{
-	/// 	error::{
-	/// 		v0::{assert_runtime_error, RuntimeError},
-	/// 		Assets,
-	/// 		AssetsError::AssetNotLive,
-	/// 	}
-	/// };
-	///
-	/// /// Example `pop-drink` testing method to interact with PSP22 contract.
-	/// fn mint(session: &mut Session<Pop>, account: AccountId, amount: Balance) -> Result<(), PSP22Error> {
-	/// 	call::<Pop, (), PSP22Error>(
-	/// 		session,
-	/// 		"Psp22Mintable::mint",
-	/// 		vec![account.to_string(), amount.to_string()],
-	/// 		None,
-	/// 	)
-	/// }
-	///
-	/// /// Using macro to test the returned error.
-	/// assert_runtime_error!(
-	/// 	mint(&mut session, BOB, AMOUNT),
-	/// 	RuntimeError::Module(Assets(AssetNotLive))
-	/// );
-	/// ```
-	#[macro_export]
-	macro_rules! __assert_runtime_err {
-		($result:expr, $error:expr $(,)?) => {
-			$crate::error::v0::assert_runtime_err_inner::<_, _, _>($result, $error);
-		};
-	}
-
-	pub use __assert_runtime_err as assert_runtime_err;
+/// A utility macro to assert that an error returned from a smart contract method using API
+/// V0.
+///
+/// # Parameters:
+///
+/// - `result` - The result returned by a smart contract method. It is of type `Result<R, E>`, where
+///   the error type `E` must implement a conversion to `u32`.
+/// - `error` - A `Error` type configured specifically for the API V0.
+///
+/// # Example:
+///
+/// ```rs
+/// use drink::devnet::{
+/// 	error::{
+/// 		v0::{assert_err, Error},
+/// 		Assets,
+/// 		AssetsError::AssetNotLive,
+/// 	}
+/// };
+///
+/// /// Example `pop-drink` testing method to interact with PSP22 contract.
+/// fn transfer(session: &mut Session<Pop>, to: AccountId, amount: Balance) -> Result<(), PSP22Error> {
+/// 	call::<Pop, (), PSP22Error>(
+/// 		session,
+/// 		"Psp22::transfer",
+/// 		vec![to.to_string(), amount.to_string(), serde_json::to_string::<[u8; 0]>(&[]).unwrap()],
+/// 		None,
+/// 	)
+/// }
+///
+/// /// Using macro to test the returned error.
+/// assert_err!(
+/// 	mint(&mut session, BOB, AMOUNT),
+/// 	Error::Module(Assets(AssetNotLive))
+/// );
+/// ```
+#[macro_export]
+macro_rules! assert_err {
+	($result:expr, $error:expr $(,)?) => {
+		$crate::error::assert_err_inner::<_, _, _>($result, $error);
+	};
 }
 
 /// A utility macro to assert that an error returned from a smart contract method matches the
-/// `RuntimeError`.
+/// `Error`.
 ///
 /// # Generic parameters:
 ///
-/// - `VersionedApiError` - Version of Pop API.
 /// - `R` - Success type returned if Ok().
 /// - `E` - Returned `Err()` value of a method result. Must be convertable to `u32`.
-/// - `RuntimeError` - Runtime error type.
+/// - `Error` - Runtime error type.
 ///
 /// # Parameters:
 ///
 /// - `result` - Result returned by a smart contract method.
-/// - `expected_error` - `RuntimeError` to be asserted.
+/// - `expected_error` - `Error` to be asserted.
 #[track_caller]
-pub fn assert_runtime_err_inner<VersionedApiError, R, E, RuntimeError>(
-	result: Result<R, E>,
-	expected_error: RuntimeError,
-) where
-	VersionedApiError: Into<u32>,
+pub fn assert_err_inner<R, E, Error>(result: Result<R, E>, expected_error: Error)
+where
 	E: Into<u32>,
-	RuntimeError: From<u32> + Into<u32> + Debug,
+	Error: From<u32> + Into<u32> + Debug,
 {
 	let expected_code: u32 = expected_error.into();
-	let expected_error = RuntimeError::from(expected_code);
+	let expected_error = Error::from(expected_code);
 	if let Err(error) = result {
 		let error_code: u32 = error.into();
 		if error_code != expected_code {
@@ -176,7 +151,7 @@ pub fn assert_runtime_err_inner<VersionedApiError, R, E, RuntimeError>(
 				r#"assertion `left == right` failed
   left: {:?}
  right: {:?}"#,
-				RuntimeError::from(error_code),
+				Error::from(error_code),
 				expected_error
 			);
 		}
@@ -192,47 +167,34 @@ pub fn assert_runtime_err_inner<VersionedApiError, R, E, RuntimeError>(
 
 #[cfg(test)]
 mod test {
-	use pop_primitives::v0::Error as PopApiError;
+	use pop_api::primitives::v0::Error as ApiError;
 
 	use crate::error::{AssetsError::*, BalancesError::*, *};
 
-	#[derive(Debug)]
-	struct Config;
-
-	impl RuntimeErrorConfig for Config {
-		type ModuleError = crate::mock::RuntimeError;
-		type PopApiError = PopApiError;
-
-		const MODULE_INDEX: u8 = 3;
-	}
-
-	fn test_cases() -> Vec<(RuntimeError<Config>, PopApiError)> {
+	fn test_cases() -> Vec<(Error<crate::mock::RuntimeError, ApiError, 3>, ApiError)> {
 		use frame_support::traits::PalletInfoAccess;
 		use pop_api::primitives::{ArithmeticError::*, TokenError::*};
 
 		use crate::mock::RuntimeError::*;
 		vec![
-			(RuntimeError::Raw(PopApiError::BadOrigin), PopApiError::BadOrigin),
-			(RuntimeError::Raw(PopApiError::Token(BelowMinimum)), PopApiError::Token(BelowMinimum)),
+			(Error::Api(ApiError::BadOrigin), ApiError::BadOrigin),
+			(Error::Api(ApiError::Token(BelowMinimum)), ApiError::Token(BelowMinimum)),
+			(Error::Api(ApiError::Arithmetic(Overflow)), ApiError::Arithmetic(Overflow)),
 			(
-				RuntimeError::Raw(PopApiError::Arithmetic(Overflow)),
-				PopApiError::Arithmetic(Overflow),
+				Error::Module(Assets(BalanceLow)),
+				ApiError::Module { index: crate::mock::Assets::index() as u8, error: [0, 0] },
 			),
 			(
-				RuntimeError::Module(Assets(BalanceLow)),
-				PopApiError::Module { index: crate::mock::Assets::index() as u8, error: [0, 0] },
+				Error::Module(Assets(NoAccount)),
+				ApiError::Module { index: crate::mock::Assets::index() as u8, error: [1, 0] },
 			),
 			(
-				RuntimeError::Module(Assets(NoAccount)),
-				PopApiError::Module { index: crate::mock::Assets::index() as u8, error: [1, 0] },
+				Error::Module(Balances(VestingBalance)),
+				ApiError::Module { index: crate::mock::Balances::index() as u8, error: [0, 0] },
 			),
 			(
-				RuntimeError::Module(Balances(VestingBalance)),
-				PopApiError::Module { index: crate::mock::Balances::index() as u8, error: [0, 0] },
-			),
-			(
-				RuntimeError::Module(Balances(LiquidityRestrictions)),
-				PopApiError::Module { index: crate::mock::Balances::index() as u8, error: [1, 0] },
+				Error::Module(Balances(LiquidityRestrictions)),
+				ApiError::Module { index: crate::mock::Balances::index() as u8, error: [1, 0] },
 			),
 		]
 	}
@@ -247,12 +209,9 @@ mod test {
 	}
 
 	#[test]
-	fn assert_runtime_error_works() {
+	fn assert_err_works() {
 		test_cases().into_iter().for_each(|t| {
-			crate::error::v0::assert_runtime_err!(
-				Result::<(), pop_primitives::Error>::Err(t.1),
-				t.0,
-			);
+			crate::assert_err!(Result::<(), pop_api::primitives::v0::Error>::Err(t.1), t.0,);
 		});
 	}
 }
--- crates/pop-drink/src/lib.rs
+++ crates/pop-drink/src/lib.rs
@@ -2,13 +2,13 @@ pub use drink::*;
 pub use frame_support::{self, assert_ok};
 pub use ink_sandbox::api::assets_api::AssetsAPI;
 use ink_sandbox::{AccountIdFor, BalanceFor};
+use scale::Decode;
 pub use session::{error::SessionError, ContractBundle, Session, NO_SALT};
 pub use sp_io::TestExternalities;
 
 pub mod error;
 #[cfg(test)]
 pub mod mock;
-pub mod utils;
 
 pub mod devnet {
 	pub use pop_runtime_devnet::Runtime;
@@ -24,21 +24,10 @@ pub mod devnet {
 		/// A collection of error types from the `v0` module used for smart contract testing in the
 		/// `devnet` environment.
 		pub mod v0 {
-			use super::*;
-			pub use crate::error::v0::*;
+			pub use pop_api::primitives::v0::{Error as ApiError, *};
 
-			/// Configuration for the runtime error used to test smart contract.
-			#[derive(Debug)]
-			pub struct Config;
-			impl RuntimeErrorConfig for Config {
-				type ModuleError = pop_runtime_devnet::RuntimeError;
-				type PopApiError = pop_primitives::v0::Error;
-
-				const MODULE_INDEX: u8 = 3;
-			}
-
-			/// Runtime error for testing contracts using the Pop API V0.
-			pub type RuntimeError = crate::error::RuntimeError<Config>;
+			/// Error type for testing contracts using the API V0.
+			pub type Error = crate::error::Error<pop_runtime_devnet::RuntimeError, ApiError, 3>;
 		}
 	}
 
@@ -46,10 +35,116 @@ pub mod devnet {
 	pub type Balance = BalanceFor<Runtime>;
 	pub type AccountId = AccountIdFor<Runtime>;
 
-	// This is used to resolve type mismatches between the `AccountId` in the quasi testing
-	// environment and the contract environment.
+	/// This is used to resolve type mismatches between the `AccountId` in the quasi testing
+	/// environment and the contract environment.
 	pub fn account_id_from_slice(s: &AccountId) -> pop_api::primitives::AccountId {
 		let account: [u8; 32] = s.clone().into();
-		utils::account_id_from_slice(&account)
+		super::account_id_from_slice(&account)
 	}
+}
+
+/// Deploys a contract with a given constructo methodr, arguments, salt and initial value. In case
+/// of success, returns the address of the deployed contract.
+///
+/// # Generic Parameters:
+///
+/// - `S`: Sandbox defines the API of a sandboxed runtime.
+/// - `E`: Decodable error type returned if the contract deployment fails.
+///
+/// # Parameters:
+///
+/// - `session` - Wrapper around `Sandbox`` that provides a convenient API for interacting with
+///   multiple contracts.
+/// - `bundle` - A struct representing the result of parsing a `.contract` bundle file.
+/// - `method` - The name of the constructor method.
+/// - `input` - Arguments passed to the constructor method.
+/// - `salt` - Additional data used to influence the contract deployment address.
+/// - `init_value` - Initial funds allocated for the contract.
+pub fn deploy<S, E>(
+	session: &mut Session<S>,
+	bundle: ContractBundle,
+	method: &str,
+	input: Vec<String>,
+	salt: Vec<u8>,
+	init_value: Option<BalanceFor<S::Runtime>>,
+) -> Result<AccountIdFor<S::Runtime>, E>
+where
+	S: Sandbox,
+	S::Runtime: pallet_contracts::Config,
+	E: Decode,
+{
+	let result = session.deploy_bundle(bundle, method, &input, salt, init_value);
+	if result.is_err() {
+		let deployment_result = session.record().last_deploy_result().result.clone();
+		let error = deployment_result.unwrap().result.data;
+		return Err(E::decode(&mut &error[2..]).unwrap());
+	}
+	Ok(result.unwrap())
+}
+
+/// Call a contract method and decode the returned data.
+///
+/// # Generic Parameters:
+///
+/// - `S` - Sandbox defines the API of a sandboxed runtime.
+/// - `O` - Decodable output type returned if the contract deployment succeeds.
+/// - `E` - Decodable error type returned if the contract deployment fails.
+///
+/// # Parameters:
+///
+/// - `session` - Wrapper around `Sandbox`` that provides a convenient API for interacting with
+///   multiple contracts.
+/// - `func_name` - The name of the contract method.
+/// - `input` - Arguments passed to the contract method.
+/// - `endowment` - Funds allocated for the contract execution.
+pub fn call<S, O, E>(
+	session: &mut Session<S>,
+	func_name: &str,
+	input: Vec<String>,
+	endowment: Option<BalanceFor<S::Runtime>>,
+) -> Result<O, E>
+where
+	S: Sandbox,
+	S::Runtime: pallet_contracts::Config,
+	O: Decode,
+	E: Decode,
+{
+	match session.call::<String, ()>(func_name, &input, endowment) {
+		// If the call is reverted, decode the error into the specified error type.
+		Err(SessionError::CallReverted(error)) => {
+			Err(E::decode(&mut &error[2..]).unwrap_or_else(|_| panic!("Decoding failed")))
+		},
+		// If the call is successful, decode the last returned value.
+		Ok(_) => Ok(session
+			.record()
+			.last_call_return_decoded::<O>()
+			.unwrap_or_else(|_| panic!("Expected a return value"))
+			.unwrap_or_else(|_| panic!("Decoding failed"))),
+		// Catch-all for unexpected results.
+		_ => panic!("Expected call to revert or return a value"),
+	}
+}
+
+fn account_id_from_slice(s: &[u8; 32]) -> pop_api::primitives::AccountId {
+	pop_api::primitives::AccountId::decode(&mut &s[..]).expect("Should be decoded to AccountId")
+}
+
+/// Get the last event from pallet contracts.
+///
+/// # Generic Parameters:
+///
+/// - `S` - Sandbox defines the API of a sandboxed runtime.
+///
+/// # Parameters:
+///
+/// - `session` - Wrapper around `Sandbox`` that provides a convenient API for interacting with
+///   multiple contracts.
+pub fn last_contract_event<S>(session: &Session<S>) -> Option<Vec<u8>>
+where
+	S: Sandbox,
+	S::Runtime: pallet_contracts::Config,
+	<S::Runtime as frame_system::Config>::RuntimeEvent:
+		TryInto<pallet_contracts::Event<S::Runtime>>,
+{
+	session.record().last_event_batch().contract_events().last().cloned()
 }
