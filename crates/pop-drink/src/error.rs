4ac3fab80b34b6cd7936b200b2715288189cfcdc
Daanvdplas <daanvdplas@live.nl>
2024-10-16 23:11:30 +0200

    docs: drink

--- crates/pop-drink/src/error.rs
+++ crates/pop-drink/src/error.rs
@@ -1,63 +1,181 @@
-//! A set of errors used for testing smart contracts.
+//! The error type and utilities for testing smart contracts using the Pop API.
 
 use std::fmt::Debug;
+use scale::{Decode, Encode};
 
 pub use drink::{
 	pallet_assets::Error as AssetsError, pallet_balances::Error as BalancesError,
 	pallet_contracts::Error as ContractsError,
 };
-use scale::{Decode, Encode};
 
-fn decode<T: Decode>(data: &[u8]) -> T {
-	T::decode(&mut &data[..]).expect("Decoding failed")
+/// A simplified error type representing errors from the runtime and its modules.
+///
+/// This type can be used to assert to an error that holds a [status code](https://github.com/r0gue-io/pop-node/blob/main/pop-api/src/lib.rs#L33).
+/// The status code is returned by the Pop API and represents a runtime error.
+///
+/// # Generic Parameters:
+/// - `ApiError`: The pop api error type.
+/// - `ModuleError`: The error type for specific runtime modules.
+/// - `MODULE_INDEX`: Index of the variant `Error::Module`.
+#[derive(Encode, Decode, Debug)]
+pub enum Error<ApiError, ModuleError, const MODULE_INDEX: u8>
+	where
+		ApiError: Decode + Encode + Debug + From<u32> + Into<u32>,
+		ModuleError: Decode + Encode + Debug,
+{
+	/// An error not related to any specific module.
+	Raw(ApiError),
+	/// An error originating from a runtime module.
+	Module(ModuleError),
 }
 
-/// Asserts a custom error type against the `Error`. This is useful when you want to check if a contract's custom error (e.g., [`PSP22Error`](https://github.com/r0gue-io/pop-node/blob/main/pop-api/src/v0/fungibles/errors.rs#L73C1-L73C22)) matches the error code returned by the runtime, which is represented by a [`StatusCode`](https://github.com/r0gue-io/pop-node/blob/main/pop-api/src/lib.rs#L33). The error type must be convertible to a `u32`.
+impl<ApiError, ModuleError, const MODULE_INDEX: u8>
+From<Error<ApiError, ModuleError, MODULE_INDEX>> for u32
+	where
+		ApiError: Decode + Encode + Debug + From<u32> + Into<u32>,
+		ModuleError: Decode + Encode + Debug,
+{
+	/// Converts an `Error` to a `u32` status code.
+	fn from(error: Error<ApiError, ModuleError, MODULE_INDEX>) -> Self {
+		match error {
+			Error::Raw(error) => decode::<ApiError>(&error.encode()),
+			Error::Module(error) => {
+				let mut encoded = error.encode();
+				encoded.insert(0, MODULE_INDEX);
+				encoded.resize(4, 0);
+				decode::<ApiError>(&encoded)
+			},
+		}
+			.into()
+	}
+}
+
+impl<ApiError, ModuleError, const MODULE_INDEX: u8> From<u32>
+for Error<ApiError, ModuleError, MODULE_INDEX>
+	where
+		ApiError: Decode + Encode + Debug + From<u32> + Into<u32>,
+		ModuleError: Decode + Encode + Debug,
+{
+	/// Converts a `u32` into an `Error`.
+	///
+	/// If the status code represents a module error it converts it into `Error::Module` in stead
+	/// of `Error::Raw`.
+	fn from(value: u32) -> Self {
+		let error = ApiError::from(value);
+		let encoded = error.encode();
+		if encoded[0] == MODULE_INDEX {
+			let (index, module_error) = (encoded[1], &encoded[2..]);
+			let data = vec![vec![index], module_error.to_vec()].concat();
+			return Error::Module(decode(&data));
+		}
+		Error::Raw(error)
+	}
+}
+
+/// Asserts that a result matches an expected `Error`.
 ///
-/// # Parameters
+/// This can be used to assert that a contract execution resulted in a specific runtime error `Error`.
+/// The contract error must be convertible to a `u32` (i.e. the status code received from the api).
 ///
-/// - `result` - Contract method's result returns the custom error type which is convertible to
-///   `u32`.
-/// - `error` - `Error` to assert against the custom error type.
+/// # Example
 ///
-/// # Examples
-///
-/// To assert the `StatusCode` returned by a contract method that uses Pop API, you simply use the
-/// provided `assert_err!` macro.
+/// ## Errors
 ///
 /// ```rs
-/// // Required imports to test the custom error.
-/// use drink::{ assert_err, devnet::error::{ v0::Error, Arithmetic, ArithmeticError::Overflow }};
-///
-/// // Call a contract method named "hello_world" that throws `StatusCode`.
-/// let result = call::<Pop, (), StatusCode>(session, "hello_world", vec![], None);
-///
-/// // Asserts that the call fails because of an arithmetic operation error.
-/// assert_err!(result, Error::Raw(Arithmetic(Overflow)));
+/// use drink::devnet::{
+/// 	Assets,
+/// 	AssetsError::BalanceLow,
+/// 	v0::{
+/// 		Arithmetic,
+/// 		ArithmeticError::Overflow,
+/// 		BadOrigin
+/// 	},
+/// };
 /// ```
+///
+/// [`BadOrigin`](https://github.com/r0gue-io/pop-node/blob/main/primitives/src/lib.rs#L36C4-L36C18):
+/// ```rs
+/// Error::Raw(BadOrigin)
+/// ```
+///
+/// [`Arithmetic(Overflow)`](https://github.com/r0gue-io/pop-node/blob/main/primitives/src/lib.rs#L55):
+/// ```rs
+/// Error::Raw(Arithmetic(Overflow))
+/// ```
+///
+/// [`Assets(BalanceLow)`](https://paritytech.github.io/polkadot-sdk/master/pallet_assets/pallet/enum.Error.html#variant.BalanceLow):
+/// ```rs
+/// Error::Module(Assets(BalanceLow))
+/// ```
+///
+/// ## How to use `assert_err` macro.
+///
+/// - Create a custom error type that holds the status code.
+///
+/// ```rs
+/// use pop_api::StatusCode;
+///
+/// /// Custom error in contract.
+/// pub enum CustomError {
+///		...,
+/// 	/// Error with status code.
+/// 	StatusCode(u32),
+/// }
+///
+/// impl From<StatusCode> for CustomError {
+/// 	/// Converts a `StatusCode` (returned by the api) to a `CustomError`.
+/// 	fn from(value: StatusCode) -> Self {
+/// 		match value {
+/// 			...,
+/// 			_ => CustomError::StatusCode(value.0),
+/// 		}
+/// 	}
+/// }
+///
+/// impl From<CustomError> for u32 {
+/// 	/// Converts a `CustomError to a `u32`.
+/// 	//
+/// 	// Required for the `assert_err` macro to assert to `Error`.
+/// 	fn from(value: CustomError) -> Self {
+/// 		match value {
+/// 			...,
+/// 			CustomError::StatusCode(status_code) => status_code,
+/// 		}
+/// 	}
+/// }
+///
+/// - Use `assert_err` in a test.
+///
+/// #[drink::test(sandbox = Pop)]
+/// fn test_custom_error(mut session: Session) {
+/// 	...
+///
+/// 	// Call a contract method that returns a `Result<(), CustomError>`.
+///		let result = call::<Pop, (), CustomError>(session, "hello_world", vec![], None);
+///
+/// 	// Assert the result to the expected error.
+/// 	assert_err!(result, Error::Raw(BadOrigin)));
+///
+/// 	// Other assertions:
+/// 	...
+/// 	assert_err!(result, Error::Raw(Arithmetic(Overflow)));
+/// 	...
+/// 	assert_err!(result, Error::Module(Assets(BalanceLow)));
+/// }
+/// ```
+///
+/// # Parameters:
+/// - `result`: The result which contains the custom error type.
+/// - `error`: The expected error.
 #[macro_export]
 macro_rules! assert_err {
-	($result:expr, $error:expr $(,)?) => {
-		$crate::error::assert_err_inner::<_, _, _>($result, $error);
-	};
+    ($result:expr, $error:expr $(,)?) => {
+        $crate::error::assert_err_inner::<_, _, _>($result, $error);
+    };
 }
 
-/// Asserts an error type to Error. This can be used for custom error types used by a contract which
-/// uses the [`StatusCode`](https://github.com/r0gue-io/pop-node/blob/main/pop-api/src/lib.rs#L33) returned by the pop runtime. The error type must be convertible to a `u32`.
-///
-/// # Generic parameters
-///
-/// - `R` - Type returned if `result` is `Ok()`.
-/// - `E` - Type returned if `result` is `Err()`. Must be convertible to `u32`.
-/// - `Error` - `Error` to assert against the custom error type.
-///
-/// # Parameters
-///
-/// - `result` - Contract method's result returns the custom error type which is convertible to
-///   `u32`.
-/// - `expected_error` - `Error` to assert against the custom error type.
 #[track_caller]
-pub fn assert_err_inner<R, E, Error>(result: Result<R, E>, expected_error: Error)
+fn assert_err_inner<R, E, Error>(result: Result<R, E>, expected_error: Error)
 where
 	E: Into<u32>,
 	Error: From<u32> + Into<u32> + Debug,
@@ -85,174 +203,56 @@ where
 	}
 }
 
-/// Error type for testing why a runtime call fails.
-///
-/// # Generic Parameters
-///
-/// - [`RuntimeCallError`](https://github.com/r0gue-io/pop-node/blob/main/primitives/src/lib.rs#L30)
-///   - Reason why a runtime call fails. (.e.g, arithmetic operation errors)
-/// - [`ModuleError`](https://paritytech.github.io/polkadot-sdk/master/solochain_template_runtime/enum.Error.html)
-///   - Refers to specific reasons a runtime call fails, originating from the runtime modules.
-/// - [`MODULE_INDEX`](https://github.com/r0gue-io/pop-node/blob/main/primitives/src/lib.rs#L38)
-/// 		- Index of the variant `Error::Module`.
-///
-/// # Examples
-///
-/// ### Runtime call errors
-///
-/// - Import types to construct runtime call errors:
-///
-/// ```rs
-/// use drink::devnet::v0::{
-/// 	Arithmetic,
-/// 	ArithmeticError::Overflow,
-/// 	BadOrigin
-/// };
-/// ```
-///
-/// - Runtime call error [`Arithmetic(Overflow)`](https://github.com/r0gue-io/pop-node/blob/main/primitives/src/lib.rs#L55):
-///
-/// ```rs
-/// Error::Raw(Arithmetic(Overflow))
-/// ```
-///
-/// - Runtime call error [`BadOrigin`](https://github.com/r0gue-io/pop-node/blob/main/primitives/src/lib.rs#L36C4-L36C18):
-///
-/// ```rs
-/// Error::Raw(BadOrigin)
-/// ```
-///
-/// ### Runtime module errors
-///
-/// - Import types to construct runtime module errors:
-///
-/// ```rs
-/// use drink::devnet::{
-/// 	Assets,
-/// 	AssetsError::AssetNotLive,
-/// 	Balances::BelowMinimum,
-/// 	BalancesError::BelowMinimum
-/// };
-/// ```
-///
-/// - Construct a runtime module error [`Assets(AssetNotLive)`](https://paritytech.github.io/polkadot-sdk/master/pallet_assets/pallet/enum.Error.html#variant.AssetNotLive):
-///
-/// ```rs
-/// Error::Module(Assets(AssetNotLive))
-/// ```
-///
-/// - Construct a runtime module error [`Balances(InsufficientBalance)`](https://docs.rs/pallet-balances/latest/pallet_balances/pallet/enum.Error.html#variant.InsufficientBalance):
-///
-/// ```rs
-/// Error::Module(Balances(InsufficientBalance))
-/// ```
-
-#[derive(Encode, Decode, Debug)]
-pub enum Error<ModuleError, RuntimeCallError, const MODULE_INDEX: u8>
-where
-	RuntimeCallError: Decode + Encode + Debug + From<u32> + Into<u32>,
-	ModuleError: Decode + Encode + Debug,
-{
-	/// Reason why a runtime call fails. [Reference](https://github.com/r0gue-io/pop-node/blob/52fb7f06a89955d462900e33d2b9c9170c4534a0/primitives/src/lib.rs#L30).
-	Raw(RuntimeCallError),
-	/// [Module error](https://paritytech.github.io/polkadot-sdk/master/solochain_template_runtime/enum.Error.html) refers to specific reasons a runtime call fails, originating from the runtime modules.
-	Module(ModuleError),
-}
-
-impl<ModuleError, RuntimeCallError, const MODULE_INDEX: u8>
-	From<Error<ModuleError, RuntimeCallError, MODULE_INDEX>> for u32
-where
-	RuntimeCallError: Decode + Encode + Debug + From<u32> + Into<u32>,
-	ModuleError: Decode + Encode + Debug,
-{
-	/// Converts an `Error` to a `u32` number.
-	///
-	/// This conversion is necessary for comparing `Error` instances with other types.
-	// Compared types must implement `Into<u32>`, as `Error` does not implement `Eq`.
-	// Use this function to obtain a numerical representation of the error for comparison or
-	// further processing.
-	fn from(error: Error<ModuleError, RuntimeCallError, MODULE_INDEX>) -> Self {
-		match error {
-			Error::Raw(error) => decode::<RuntimeCallError>(&error.encode()),
-			Error::Module(error) => {
-				let mut encoded = error.encode();
-				encoded.insert(0, MODULE_INDEX);
-				encoded.resize(4, 0);
-				decode::<RuntimeCallError>(&encoded)
-			},
-		}
-		.into()
-	}
-}
-
-impl<ModuleError, RuntimeCallError, const MODULE_INDEX: u8> From<u32>
-	for Error<ModuleError, RuntimeCallError, MODULE_INDEX>
-where
-	RuntimeCallError: Decode + Encode + Debug + From<u32> + Into<u32>,
-	ModuleError: Decode + Encode + Debug,
-{
-	/// Converts a numerical value `u32` into an `Error`.
-	///
-	/// This is used to reconstruct and display an `Error` from its numerical representation
-	/// when an error is thrown.
-	fn from(value: u32) -> Self {
-		let error = RuntimeCallError::from(value);
-		let encoded = error.encode();
-		if encoded[0] == MODULE_INDEX {
-			let (index, module_error) = (encoded[1], &encoded[2..]);
-			let data = vec![vec![index], module_error.to_vec()].concat();
-			return Error::Module(decode(&data));
-		}
-		Error::Raw(error)
-	}
+fn decode<T: Decode>(data: &[u8]) -> T {
+	T::decode(&mut &data[..]).expect("Decoding failed")
 }
 
 #[cfg(test)]
 mod test {
-	use pop_api::primitives::v0::Error as RuntimeCallError;
+	use pop_api::primitives::v0::Error as ApiError;
 
 	use crate::error::{AssetsError::*, BalancesError::*, *};
 
-	fn test_cases() -> Vec<(Error<crate::mock::RuntimeError, RuntimeCallError, 3>, RuntimeCallError)>
+	fn test_cases() -> Vec<(Error<ApiError, crate::mock::RuntimeError, 3>, ApiError)>
 	{
 		use frame_support::traits::PalletInfoAccess;
 		use pop_api::primitives::{ArithmeticError::*, TokenError::*};
 
 		use crate::mock::RuntimeError::*;
 		vec![
-			(Error::Raw(RuntimeCallError::BadOrigin), RuntimeCallError::BadOrigin),
+			(Error::Raw(ApiError::BadOrigin), ApiError::BadOrigin),
 			(
-				Error::Raw(RuntimeCallError::Token(BelowMinimum)),
-				RuntimeCallError::Token(BelowMinimum),
+				Error::Raw(ApiError::Token(BelowMinimum)),
+				ApiError::Token(BelowMinimum),
 			),
 			(
-				Error::Raw(RuntimeCallError::Arithmetic(Overflow)),
-				RuntimeCallError::Arithmetic(Overflow),
+				Error::Raw(ApiError::Arithmetic(Overflow)),
+				ApiError::Arithmetic(Overflow),
 			),
 			(
 				Error::Module(Assets(BalanceLow)),
-				RuntimeCallError::Module {
+				ApiError::Module {
 					index: crate::mock::Assets::index() as u8,
 					error: [0, 0],
 				},
 			),
 			(
 				Error::Module(Assets(NoAccount)),
-				RuntimeCallError::Module {
+				ApiError::Module {
 					index: crate::mock::Assets::index() as u8,
 					error: [1, 0],
 				},
 			),
 			(
 				Error::Module(Balances(VestingBalance)),
-				RuntimeCallError::Module {
+				ApiError::Module {
 					index: crate::mock::Balances::index() as u8,
 					error: [0, 0],
 				},
 			),
 			(
 				Error::Module(Balances(LiquidityRestrictions)),
-				RuntimeCallError::Module {
+				ApiError::Module {
 					index: crate::mock::Balances::index() as u8,
 					error: [1, 0],
 				},
